1125f2da2c59a45910c102ef67718f3efd15c2ff
anyel-ec <paulangel826@gmail.com>
2024-08-27 12:24:01 -0500

    start project in v3.3.3

---
+++ src/main/java/top/anyel/rabbit/SpringBootRabbitMqProducerDirectApplication.java
@@ -0,0 +1,13 @@
+package top.anyel.rabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRabbitMqProducerDirectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRabbitMqProducerDirectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/anyel/rabbit/SpringBootRabbitMqProducerDirectApplicationTests.java
@@ -0,0 +1,13 @@
+package top.anyel.rabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRabbitMqProducerDirectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
